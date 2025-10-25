9d4f9274c4ef5e69ea684ef79680801315cfaf0a
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-10-25 21:32:02 +0500

    created a signup page and Updated Login page + added some JS

---
+++ signup.js
@@ -0,0 +1,15 @@
+let loginbtn = document.getElementById("loginBtn");
+let regBtn = document.getElementById("registerBtn");
+let loginform = document.getElementById("login");
+let regform = document.getElementById("register");
+
+
+function login(){
+    loginform.style.left = "4px";
+    regform.style.right = "-520px";
+}
+
+function register(){
+    loginform.style.left = "-510px";
+    regform.style.right = "5px";
+}
