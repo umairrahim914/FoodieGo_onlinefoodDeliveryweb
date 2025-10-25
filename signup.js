e3659903864bf4632a4a2ca18096c7a81165453f
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-10-25 22:52:42 +0500

    alginment set

--- signup.js
+++ signup.js
@@ -7,9 +7,11 @@ let regform = document.getElementById("register");
 function login(){
     loginform.style.left = "4px";
     regform.style.right = "-520px";
+    regform.style.marginRight = "0px";
 }
 
 function register(){
     loginform.style.left = "-510px";
     regform.style.right = "5px";
+    regform.style.marginRight = "200px";
 }
