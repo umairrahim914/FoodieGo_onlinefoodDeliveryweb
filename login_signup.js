61e986c312fcdadd8afa956cb3b1451da0428ad3
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-10-30 02:16:33 +0500

    some fixess

--- login_signup.js
+++ login_signup.js
@@ -7,11 +7,9 @@ let regform = document.getElementById("register");
 function login(){
     loginform.style.left = "4px";
     regform.style.right = "-520px";
-    regform.style.marginRight = "0px";
 }
 
 function register(){
     loginform.style.left = "-510px";
     regform.style.right = "5px";
-    regform.style.marginRight = "300px";
 }
