1158bc6f01fc3c7cd776355e1be5629c63282a23
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-12-13 17:51:01 +0500

    all done till 2:9:32

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
+      loop: true,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#next",
+        prevEl: "#prev",
       },
     });
