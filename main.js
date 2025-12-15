b13e1473702c2c9aa0a8890a9802ae15afd0dfc8
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-12-15 23:22:32 +0500

    3 25

--- main.js
+++ main.js
@@ -9,5 +9,44 @@ var swiper = new Swiper(".mySwiper",{
 
     const cartIcon = document.querySelector(".cart-icon");
     const cartTab = document.querySelector(".cart-tab");
+    const closeBtn = document.querySelector(".close-btn"); 
+    const cardList = document.querySelector(".card-list");
 
     cartIcon.addEventListener("click",()=> cartTab.classList.add("cart-tab-active"));
+
+    closeBtn.addEventListener("click",()=> cartTab.classList.remove("cart-tab-active"));
+
+    let productList = [];
+    const initApp = () =>{
+
+      fetch("products.json").then
+      (response => response.json()).then
+      (data =>{
+
+        productList = data;
+        console.log(productList);
+      })
+
+    }
+
+    initApp();
+
+    const showCards = () =>{
+      productList.forEach(product=>{
+        const orderCard = document.createElement("div");
+        orderCard.classList.add("order-card");
+
+        orderCard.innetHTML = ` 
+        <div class="card-image">
+            <img src="images/burger.png">
+        </div>
+        <h4>Double Beef Burger</h4>
+        <h4 class="price">$200</h4>
+        <a href="#"class="btn">Add to Cart</a>
+        `;
+
+          cardList.appendChild(orderCard);
+      })
+
+
+    }
