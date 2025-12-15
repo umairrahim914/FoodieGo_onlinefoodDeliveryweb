1fbbd1eb596f5494d70e6c52fa7709fed5643d29
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-12-15 22:51:01 +0500

    product.json + cartactive

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper",{
       loop: true,
       navigation: {
         nextEl: "#next",
         prevEl: "#prev",
       },
     });
+
+
+    const cartIcon = document.querySelector(".cart-icon");
+    const cartTab = document.querySelector(".cart-tab");
+
+    cartIcon.addEventListener("click",()=> cartTab.classList.add("cart-tab-active"));
