9543d09e1650204b1caa60db7395660e91e56c1d
Muhammad Umair Shaukat <uk742609@gmail.com>
2025-12-18 00:08:57 +0500

    js updated

--- main.js
+++ main.js
@@ -11,12 +11,69 @@ var swiper = new Swiper(".mySwiper",{
     const cartTab = document.querySelector(".cart-tab");
     const closeBtn = document.querySelector(".close-btn"); 
     const cardList = document.querySelector(".card-list");
+    const cartList = document.querySelector(".cart-list");
 
     cartIcon.addEventListener("click",()=> cartTab.classList.add("cart-tab-active"));
 
     closeBtn.addEventListener("click",()=> cartTab.classList.remove("cart-tab-active"));
 
     let productList = [];
+
+    
+    const showCards = () =>{
+      productList.forEach(product=>{
+        const orderCard = document.createElement("div");
+        orderCard.classList.add("order-card");
+
+        orderCard.innerHTML = ` 
+        <div class="card-image">
+            <img src="${product.image}">
+        </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#"class="btn card-btn">Add to Cart</a>
+        `;
+
+          cardList.appendChild(orderCard);
+
+          const cardBtn = orderCard.querySelector(".card-btn")
+          cardBtn.addEventListener("click",(e)=>{
+            e.preventDefault(); //stops the page for loading (page will not refresh)
+
+            addToCart();
+            
+
+          })
+      });
+    };
+
+    const addToCart = () =>{
+      const cartItem = document.createElement("div");
+      cartItem.classList.add("item");
+
+      cartItem.innerHTML = `
+        <div class="item-image">
+              <img src="images/burger.png">
+            </div>
+            <div>
+              <h4>Double Beef Burger</h4>
+              <h4 class="item-total">$200</h4>
+            </div>
+            <div class="flex">
+              <a href="#"class="quantity-btn">
+                <i class="fa-solid fa-minus"></i>
+              </a>
+              <h4 class="quantity-value">1</h4>
+              <a href="#"class="quantity-btn">
+                <i class="fa-solid fa-plus"></i>
+              </a>
+            </div>
+      
+      `;
+
+      cartList.appendChild(cartItem);
+
+    };
     const initApp = () =>{
 
       fetch("products.json").then
@@ -24,29 +81,10 @@ var swiper = new Swiper(".mySwiper",{
       (data =>{
 
         productList = data;
-        console.log(productList);
+        showCards();
       })
 
     }
 
     initApp();
 
-    const showCards = () =>{
-      productList.forEach(product=>{
-        const orderCard = document.createElement("div");
-        orderCard.classList.add("order-card");
-
-        orderCard.innetHTML = ` 
-        <div class="card-image">
-            <img src="images/burger.png">
-        </div>
-        <h4>Double Beef Burger</h4>
-        <h4 class="price">$200</h4>
-        <a href="#"class="btn">Add to Cart</a>
-        `;
-
-          cardList.appendChild(orderCard);
-      })
-
-
-    }
